406dd3cf7ae474ccfcb2e933a91c541beb06e779
RUS <rus_04@mail.ru>
2024-02-26 21:12:58 +0300

    hw 2 - done

--- SearchTests.java
+++ SearchTests.java
@@ -1,9 +1,3 @@
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 public class SearchTests {
     @Test
     void successfulSearchTest() {
